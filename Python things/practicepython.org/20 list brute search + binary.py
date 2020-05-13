f3495317e583d461014ffb9380c4c99a866edc75
ddoox <dudek746@gmail.com>
2020-05-13 21:30:10 +0200

    Binary search

---
+++ Python things/practicepython.org/20 list brute search + binary.py
@@ -0,0 +1,42 @@
+#TODO: binary on indexes
+def binary(_list, _element):
+    found = False
+
+    while len(_list) > 1:
+        center = _list[int((len(_list) - 1) / 2)]
+
+        if _element == center:   # has center
+            found = True
+            break
+
+        elif _element > center:
+            _list = _list[int((len(_list) - 1) / 2) + 1::+1]
+
+        else:                   # _element < center
+            _list = _list[0:int((len(_list) - 1) / 2):1]
+
+    if _list[0] == _element:
+        found = True
+
+    return found
+
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 33, 88, 95, 123, 456, 789]
+
+number = 33
+
+print(binary(list, number))
+
+
+# contain = False
+#
+# for element in list:
+#     if element == number:
+#         print("yep")
+#         contain = True
+#         break
+#
+# if not contain:
+#     print("nope")
+#
+
