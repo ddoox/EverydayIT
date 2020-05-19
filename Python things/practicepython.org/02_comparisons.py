affaf8f0cd23e4fb79bf4f9f16b5c9f2170a75c4
ddoox <dudek746@gmail.com>
2020-05-19 08:37:57 +0200

    Rename files

--- Python things/practicepython.org/02_comparisons.py
+++ Python things/practicepython.org/02_comparisons.py
@@ -8,7 +8,7 @@ num = int(input())
 if num > 10:
     print("true")
     print("no staples...")
-elif num < 10 and num > 5:
+elif 10 > num > 5:
     print("elif??? 2 letters? Why not &&??? One letter less...")
 else:
     print("And there is else :)")
