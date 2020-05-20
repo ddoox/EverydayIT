2b6c6deaa415c9e88fe2fb4e11e77a308315d121
ddoox <dudek746@gmail.com>
2020-05-20 22:33:00 +0200

    Busy day

--- Python things/practicepython.org/34_JSON.py
+++ Python things/practicepython.org/34_JSON.py
@@ -1,24 +1,34 @@
 import json
 
-#TODO: do some things with it
-json_data = {
-    "name": "Michele",
-    "shirt_color": "blue",
-    "laptops": [{
-        "brand": "Lenovo",
-        "operating_system": "Ubuntu"
-    },
-        {
-            "brand": "Apple",
-            "operating_system": "OSX"
-        }
-    ],
-    "has_a_dog": False,
-    "items_on_desk": ["mug", "pen", "monitor"]
-}
 
-with open("34.json", "w") as json_file:
-    json.dump(json_data, json_file)   # auto convert False -> false
+# json_data = {
+#     "name1": "date1",
+#     "name2": "date2",
+#     "name3": "date3",
+#     "name4": "date4",
+#     "name5": "date5",
+#     "name6": "date6",
+#     "name7": "date7",
+#     "name8": "date8,",
+#     "name9": "date9"
+# }
 
-print(json_data.keys())
 
+# with open("34.json", "w") as json_file:
+#     json.dump(json_data, json_file)   # auto convert False -> false
+#
+# print(json_data.keys())
+
+
+with open("34.json", "r") as json_file:
+    json_data = json.load(json_file)
+
+    print("Welcome to the price dictionary. We know the price of:")
+    for name in json_data.keys():
+        print(name)
+
+    name = input("\nWho's birthday do you want to look up? ")
+    if json_data.get(name):
+        print("\nPrice of {} is: {}".format(name, json_data.get(name)))   # can't print float, format like printf
+    else:
+        print("Price not found")
